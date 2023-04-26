7302b4a7e5e8ff8b2a81bc1a0a9bdd8fd587d591
Peyton Cleveland <peyton.cleveland@omnifederal.com>
2023-04-26 11:51:12 -0500

    missed file

--- exercise-1/index.js
+++ exercise-1/index.js
@@ -1,5 +1,5 @@
 // Exercise 1
-// Retrieve an array of numbers from http://localhost:3005/api/exercise-1 and return the sum of all odd numbers in the array.
+// Retrieve an array of numbers from http://localhost:3005/api/exercise-1 using fetch() and return the sum of all odd numbers in the array.
 //
 
 const sumOddNumbers = async () => {
